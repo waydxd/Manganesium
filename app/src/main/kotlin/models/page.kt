cad262759d50c0e41e9b1c1708d0adeb1ada933a
WL-jpg <wywleung@connect.ust.hk>
2025-03-15 17:44:12 +0800

    class Page

--- app/src/main/kotlin/models/page.kt
+++ app/src/main/kotlin/models/page.kt
@@ -1,1 +1,12 @@
 package org.manganesium.models
+
+data class Page(
+    val id: Long,
+    val url: String,
+    val title: String?,
+    val content: String,
+    val lastModified: String?,
+    val size: Int,
+    val keywords: Map<String, Int>,
+    val links: List<String>
+)
