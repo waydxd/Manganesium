243731972798caba0a5c0448863a97cb1a7e39a1
Wayd <wychancf@connect.ust.hk>
2025-03-20 16:44:15 +0800

    Add SLF4J dependency to build.gradle.kts for improved logging support

--- search/build.gradle.kts
+++ search/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(project(":utils"))  // Add dependency to utils module
     testImplementation(kotlin("test"))
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3") // Kotlin logging dependency
+    implementation("org.slf4j:slf4j-simple:2.0.3") // SLF4J dependency
+
 }
 
 tasks.test {
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.mapdb:mapdb:3.0.8") // MapDB dependency
     implementation("org.jsoup:jsoup:1.19.1") // Jsoup dependency
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3") // Kotlin logging dependency
+    implementation("org.slf4j:slf4j-simple:2.0.3") // SLF4J dependency
 }
 
 tasks.test {
