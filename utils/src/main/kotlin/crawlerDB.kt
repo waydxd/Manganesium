b235fa5c984c76f3e87dbe1286badf586be54d0e
Wayd <wychancf@connect.ust.hk>
2025-03-21 01:23:32 +0800

    Rename DB.kt to crawlerDB.kt and implement logging for parent-child links

--- utils/src/main/kotlin/crawlerDB.kt
+++ utils/src/main/kotlin/crawlerDB.kt
@@ -1,7 +1,6 @@
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.mapdb.DBMaker
 import org.mapdb.Serializer
-import kotlin.math.log
 
 fun main() {
     // Open the database file
@@ -11,19 +10,33 @@ fun main() {
     // Access the map you want to inspect
     val urlToPageIdMap = db.hashMap("url_to_page_id", Serializer.STRING, Serializer.STRING).createOrOpen()
     val pagePropertiesMap = db.hashMap("page_properties", Serializer.STRING, Serializer.JAVA).createOrOpen()
-
+    val parentChildLinksMap = db.treeMap("parent_child_links", Serializer.STRING, Serializer.JAVA).createOrOpen()
     // Print the contents of the urlToPageId map
-    println("Contents of urlToPageId map:")
+    var count = 0
+    logger.info { "Contents of urlToPageId map:" }
     for ((key, value) in urlToPageIdMap) {
+        if (count++ > 10) {
+            break
+        }
         logger.info {"URL: $key -> Page ID: $value"}
     }
-
+    count = 0
     // Print the contents of the pageProperties map
-    println("\nContents of pageProperties map:")
+    logger.info { "\nContents of pageProperties map:" }
     for ((key, value) in pagePropertiesMap) {
+        if (count++ > 10) {
+            break
+        }
         logger.info {"URL: $key -> Page Properties: $value"}
     }
-
+    count = 0
     // Close the database
+    logger.info {"\nContents of parent-child link map:"}
+    for ((key, value) in parentChildLinksMap ) {
+        if (count++ > 10) {
+            break
+        }
+        logger.info {"PageID: $key -> PageID(s): $value"}
+    }
     db.close()
 }
