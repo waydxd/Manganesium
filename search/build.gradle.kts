62b70e2c2f73b92d65b89caba2acac9030187575
WL-jpg <wywleung@connect.ust.hk>
2025-03-22 00:23:39 +0800

    update

--- search/build.gradle.kts
+++ search/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation(project(":utils"))  // Add dependency to utils module
     testImplementation(kotlin("test"))
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3") // Kotlin logging dependency
-    implementation("org.slf4j:slf4j-simple:2.0.3") // SLF4J dependency
+    implementation("org.slf4j:slf4j-api:2.0.3") // SLF4J dependency
 
 }
 
