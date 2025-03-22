a46ee4712bd2ad7d19489eac83d62f735a5c8856
Ryan Kwok <lifexperial@gmail.com>
2025-03-22 21:08:11 +0800

    + IndexerMain.kt

--- indexer/src/main/kotlin/org/manganesium/indexer/IndexerMain.kt
+++ indexer/src/main/kotlin/org/manganesium/indexer/IndexerMain.kt
@@ -1,13 +1,14 @@
 package org.manganesium.indexer
 
+import dataAccessObject.IndexerDAO
 import models.Page
 
 fun main() {
     val kwP = KeywordProcessor()
     val Ixer = Indexer()
-//    val indexerDao = IndexerDAO("indexer.db")
+    val indexerDao = IndexerDAO("indexer.db")
 
-    val testpage = Page("UUID", "https://www.google.com", "Google", "Google is a search engine", "2021-09-01", 100, listOf("https://www.google.com/about"))
+    val testpage = Page("UUID", "https://www.google.com", "page title", "page title is a what content", "2021-09-01", 100, listOf("https://www.google.com/about"))
 
     Ixer.indexPage(testpage)
 
